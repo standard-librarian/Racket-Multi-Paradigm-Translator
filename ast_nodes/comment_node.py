df95c97505466123335d98f6716afb2f91820178
Mina Raafat <mina.raafat62@gmail.com>
2024-05-17 00:26:52 +0300

    add comment handle

---
+++ ast_nodes/comment_node.py
@@ -0,0 +1,13 @@
+from ast_nodes.ast_node import ASTNode
+
+
+class CommentNode(ASTNode):
+    def __init__(self, comment: str):
+        self.comment = comment
+
+    def generate_python_code(self):
+        output = "#"
+        if self.comment.startswith(";"):
+            self.comment = self.comment[1:]
+        output += self.comment
+        return output
