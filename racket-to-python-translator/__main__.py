4abe8da6198eb574e71ca3d5caf4c7b2f5cdecdd
Mina Raafat <mina.raafat62@gmail.com>
2024-05-18 20:03:47 +0300

    changes in file generation

--- racket-to-python-translator/__main__.py
+++ racket-to-python-translator/__main__.py
@@ -24,6 +24,27 @@ def main(filename=None):
             file.write("")
         with open("output_code.py", "w") as file:
             file.write("")
+        with open("output_code.py", "a") as file:
+            file.write(
+                """
+from functools import reduce
+import operator
+from math import sqrt
+binary_func_all = lambda func: lambda l: reduce(func, l)
+add_all = binary_func_all(operator.add) # +
+sub_all = binary_func_all(operator.sub) # -
+mul_all = binary_func_all(operator.mul) # *
+div_all = binary_func_all(operator.truediv) # /
+
+all_binary_func = lambda func: lambda l: all([True if func(l[i -1], l[i]) else False for i in range(1, len(l))])
+all_eq = all_binary_func(operator.eq) # ==
+all_gt = all_binary_func(operator.gt) # >
+all_ge = all_binary_func(operator.ge) # >=
+all_lt = all_binary_func(operator.lt) # <
+all_le = all_binary_func(operator.le) # <=
+
+"""
+            )
         with open(filename, "r") as file:
             for line in file:
                 tokens = lexer.tokenize_line(line)
